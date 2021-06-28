ed76be1f01ebde8d729ecf40dbb505c42832272f
Ilham1104 <59250511+ilham1104@users.noreply.github.com>
2021-06-28 13:08:52 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,11 +3,9 @@
 pkg update -y
 pkg upgrade -y
 pkg install wget -y
-pkg install ffmpeg -y
 pkg install nodejs -y
 pkg install npm -y
 pkg install tesseract -y
-apt install nmap -y
 npm i
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
